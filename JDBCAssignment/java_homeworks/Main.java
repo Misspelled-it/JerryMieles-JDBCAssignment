6540960f3114d0086bed642318c3cd0cd4a1d719
Jerry Mieles <jaymieles7@gmail.com>
2017-09-05 13:04:51 -0400

    finalized server schema

--- JDBCAssignment/java_homeworks/Main.java
+++ JDBCAssignment/java_homeworks/Main.java
@@ -10,7 +10,7 @@ public class Main {
 //		Statement stmt = null;
 //		try{
 //			Class.forName("oracle.jdbc.driver.OracleDriver");
-//			String URL = "jdbc:oracle:thin:@localhost:xxxx:orcl";
+//			String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
 //			conn = DriverManager.getConnection(URL,"sys as sysdba", "password123");
 //			stmt = conn.createStatement();
 //			String sql = "CREATE TABLE Employees (EmployeeNo NUMBER(10) NOT NULL, "
@@ -18,8 +18,7 @@ public class Main {
 //					+ "lastName VARCHAR(255), "
 //					+ "time_in VARCHAR(255), "
 //					+ "time_out VARCHAR(255), "
-//					+ "CONSTRAINT Employees_pk PRIMARY KEY (EmployeeNo), "
-//					+ "CONSTRAINT Employees_no_uq UNIQUE (EmployeeNo)) ";
+//					+ "CONSTRAINT Employees_pk PRIMARY KEY (EmployeeNo));
 //			stmt.executeQuery(sql);
 //			sql = "CREATE SEQUENCE employee_id_no_seq START WITH 1 "
 //					+ "INCREMENT BY 1 "
