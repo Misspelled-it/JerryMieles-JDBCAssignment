b02a129474ade3502d33daf7700320366036caa3
Jerry Mieles <jaymieles7@gmail.com>
2017-09-07 21:59:42 -0400

    added comments

--- JDBCAssignment/java_homeworks/Main.java
+++ JDBCAssignment/java_homeworks/Main.java
@@ -9,6 +9,7 @@ public class Main {
 //		Connection conn = null;
 //		Statement stmt = null;
 //		try{
+		//make a connection and create the table Employees
 //			Class.forName("oracle.jdbc.driver.OracleDriver");
 //			String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
 //			conn = DriverManager.getConnection(URL,"sys as sysdba", "password123");
@@ -20,6 +21,7 @@ public class Main {
 //					+ "time_out VARCHAR(255), "
 //					+ "CONSTRAINT Employees_pk PRIMARY KEY (EmployeeNo));
 //			stmt.executeQuery(sql);
+		//while the connection is still up make a sequence to use when inserting new employees into the table
 //			sql = "CREATE SEQUENCE employee_id_no_seq START WITH 1 "
 //					+ "INCREMENT BY 1 "
 //					+ "MINVALUE 1 "
@@ -35,6 +37,7 @@ public class Main {
 //				stmt.close();
 //			}
 //		}
+		//make an input collector and run the logic
 		inputCollector example = new inputCollector();
 		example.collectInput();
 	}
